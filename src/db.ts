409c4f26d84984510d715a0a668584836ce689f0
lvndry <lvndry@protonmail.com>
2021-10-24 14:10:52 +0200

    can pause/resume readline

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
+import { StockModel } from "./stock.interface";
 
 export const initDBConnection = async () => {
-  return await mongoose.connect("mongodb://localhost:27017/sirene", {
+  const db = await mongoose.connect("mongodb://localhost:27017/sirene", {
     appName: "sirene-app",
     ignoreUndefined: true,
     minPoolSize: 3,
   });
+
+  await StockModel.deleteMany({});
+  console.log("Delete stocks from database");
+
+  return db;
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import { createReadStream } from "fs";
-import { createInterface } from "readline";
+import { createInterface, emitKeypressEvents } from "readline";
 import os from "os";
 
 import { WorkerPool } from "./worker_pool";
@@ -10,56 +10,71 @@ import { StockModel } from "./stock.interface";
 
 console.time(__filename);
 
-const filePath = path.join(__dirname, "../stock.csv");
-const workerPath = path.join(__dirname, "./worker.js");
+async function main() {
+  const filePath = path.join(__dirname, "../stock.csv");
+  const workerPath = path.join(__dirname, "./worker.js");
 
-// Create stream reader, skip the 1305 first bytes to skip the first line
-const inputStream = createReadStream(filePath, { start: 1305 });
-const rl = createInterface({ input: inputStream });
+  // Create stream reader, skip the 1305 first bytes to skip the first line
+  const inputStream = createReadStream(filePath, { start: 1305 });
+  const rl = createInterface({ input: inputStream });
 
-let lines: string[] = [];
-let db: typeof import("mongoose");
+  emitKeypressEvents(process.stdin);
+  process.stdin.setRawMode(true);
 
-initDBConnection().then((database) => {
-  db = database;
-  StockModel.deleteMany({}).then(() =>
-    console.log("Delete stocks from database")
-  );
-});
-
-const workerPool = new WorkerPool(os.cpus().length, {
-  path: workerPath,
-  options: { workerData: { path: "./worker.ts" } },
-});
-
-const WORKER_LOAD = 1000;
-
-rl.on("line", (line) => {
-  lines = lines.concat([line]);
-
-  if (lines.length === WORKER_LOAD) {
-    const start = performance.now();
-
-    workerPool.runTask(lines, async (err, models) => {
-      const end = performance.now();
-
-      console.log(
-        `workerPool task took ${Math.trunc(end - start) / 1000} seconds`
-      );
-
-      if (err) {
-        console.error(err);
-      } else {
-        const docs = await StockModel.insertMany(models);
-        console.log(`Inserted ${docs.length} documents`);
+  process.stdin.on("keypress", (str, key) => {
+    if (key.ctrl) {
+      if (key.name === "p") {
+        console.log("Pause...");
+        rl.pause();
+      } else if (key.name === "r") {
+        console.log("Resume...");
+        rl.resume();
+      } else if (key.name === "c") {
+        console.log("Exiting program...");
+        process.exit();
       }
-    });
+    }
+  });
 
-    lines = [];
-  }
-});
+  await initDBConnection();
 
-rl.on("close", () => {
-  console.timeEnd(__filename);
-  workerPool.close();
-});
+  const workerPool = new WorkerPool(os.cpus().length, {
+    path: workerPath,
+    options: { workerData: { path: "./worker.ts" } },
+  });
+
+  const WORKER_LOAD = 1000;
+  let lines: string[] = [];
+
+  rl.on("line", (line) => {
+    lines = lines.concat([line]);
+
+    if (lines.length === WORKER_LOAD) {
+      const start = performance.now();
+
+      workerPool.runTask(lines, async (err, models) => {
+        const end = performance.now();
+
+        console.log(
+          `workerPool task took ${Math.trunc(end - start) / 1000} seconds`
+        );
+
+        if (err) {
+          console.error(err);
+        } else {
+          const docs = await StockModel.insertMany(models);
+          console.log(`Inserted ${docs.length} documents`);
+        }
+      });
+
+      lines = [];
+    }
+  });
+
+  rl.on("close", () => {
+    console.timeEnd(__filename);
+    workerPool.close();
+  });
+}
+
+main();
